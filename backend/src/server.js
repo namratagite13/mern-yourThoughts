a9c7ce47cb8d6685c0f3c0cd9090d0ed7abe5651
namratagite13 <106989148+namratagite13@users.noreply.github.com>
2025-11-06 20:10:44 +0530

    prepared for development

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
 
-// 💡 NEW POSITION: MUST run before any module imports that try to read env variables
+
 dotenv.config();
 
 import notesRoutes from './routes/notesRoutes.js'
@@ -17,40 +17,30 @@ const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
 // middleware
-// if (process.env.NODE_ENV !== "production") {
-//   app.use(
-//     cors({
-//       origin: "http://localhost:5173",
-//     })
-//   );
-// }
-
-const allowedOrigins = ['http://localhost:5173']; // Change 5173 to 5174
-
-app.use(cors({
-  origin: allowedOrigins
-}));
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 
 
 app.use(express.json()); // this middleware will parse JSON bodies: req.body
 app.use(rateLimiter);
 
-// our simple custom middleware
-// app.use((req, res, next) => {
-//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
-//   next();
-// });
 
 app.use("/api/notes", notesRoutes);
 app.use("/api/ai", searchRoutes)
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/frontend", "dist", "index.html"));
+  });
+}
+
 
 connectDB().then(() => {
   app.listen(PORT, () => {
